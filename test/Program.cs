334f5e897dfa43851a8b6742f3d42e148b5f2443
elektromin <elektromin@hotmail.com>
2013-08-28 13:14:01 +0200

    hello world

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello you virtual world!");
+            Console.Out.WriteLine("This is a bit slow, don't you think?");
+            Console.Out.WriteLine("Funkar det med svenska tecken åxå? åäöÅÄÖ$/()|[]\\{}");
+            Thread.Sleep(10000);
+        }
+    }
+}
