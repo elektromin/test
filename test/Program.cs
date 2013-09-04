dfaf2c53b475b1449ee14bd0a33b5adc2aee9d0c
elektromin <elektromin@hotmail.com>
2013-09-04 12:43:32 +0200

    stuff

--- test/Program.cs
+++ test/Program.cs
@@ -13,7 +13,27 @@ namespace test
             Console.Out.WriteLine("Hello you virtual world!");
             Console.Out.WriteLine("This is a bit slow, don't you think?");
             Console.Out.WriteLine("Funkar det med svenska tecken åxå? åäöÅÄÖ$/()|[]\\{}");
+            var people = new List<Person>
+                             {
+                                 new Person() {Name = "Arne Anka", Gender = Gender.Male},
+                                 new Person() {Name = "Kajsa Anka", Gender = Gender.Female}
+                             };
+            foreach (var person in people)
+            {
+                Console.Out.WriteLine(person.Name + ", " + person.Gender);
+            }
             Thread.Sleep(10000);
         }
+    }
+
+    internal class Person
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    enum Gender
+    {
+        Male, Female
     }
 }
